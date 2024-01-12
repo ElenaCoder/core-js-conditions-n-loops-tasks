456d500d3c7fc8015d6f68f2162c69bcaed99b91
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-01-12 22:33:44 +0200

    feat: add 1-4 task's solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,16 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(...theArgs) {
+  let max = theArgs[0];
+
+  for (let i = 1; i < theArgs.length; i += 1) {
+    if (max < theArgs[i]) {
+      max = theArgs[i];
+    }
+  }
+
+  return max;
 }
 
 /**
@@ -60,8 +68,13 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  const rowDiff = Math.abs(queen.x - king.x);
+  const colDiff = Math.abs(queen.y - king.y);
+
+  const result =
+    rowDiff === colDiff || queen.x === king.x || queen.y === king.y;
+  return result;
 }
 
 /**
@@ -82,8 +95,30 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return false;
+  }
+
+  if (!(a + b > c && b + c > a && a + c > b)) {
+    return false;
+  }
+
+  let countEqualSides = 0;
+
+  if (a === b) {
+    countEqualSides += 1;
+  }
+
+  if (b === c) {
+    countEqualSides += 1;
+  }
+
+  if (a === c) {
+    countEqualSides += 1;
+  }
+
+  return countEqualSides === 1;
 }
 
 /**
